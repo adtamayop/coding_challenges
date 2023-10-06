1ba35e959a1d052a8f93006af4e4d75cc11dd2d4
adtamayop <adtamayop@unal.edu.co>
2023-10-05 21:40:28 -0500

    Counting sort 1 solution

---
+++ 1_week_preparation _kit/day_2/3_counting_sort_1/readme.md
@@ -0,0 +1,2 @@
+# Descripción problema
+- [Problema 3: Counting Sort 1](https://www.hackerrank.com/challenges/one-week-preparation-kit-countingsort1/problem)
---
+++ 1_week_preparation _kit/day_2/3_counting_sort_1/sol_tamayo.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    
+    freq_list = [0]*100
+    
+    for item in arr:
+        
+        freq_list[item]+= 1
+        
+    return freq_list
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
